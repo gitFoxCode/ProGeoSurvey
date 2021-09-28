67768e65b266017cfc40f4f60beaecab4c27207c
Sebastian <sebeklis132@gmail.com>
2021-09-28 21:09:30 +0200

    fonts

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,10 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {rel: 'preconnect', href: 'https://fonts.googleapis.com'},
+      {rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true},
+      {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;400;500;600;700;800;900&display=swap', crossorigin: true},
     ]
   },
 
